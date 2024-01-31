603379723eec891081dd5d5ebaae5ce853ad39d7
Tubshinbat~ <tuvshinbat596@gmail.com>
2024-01-31 15:19:30 +0800

    a

--- axios-base.js
+++ axios-base.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/v1/",
-  // baseURL: "https://naog.lvg.mn/api/",
+  // baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://naog.lvg.mn/api/",
 });
 
 instance.defaults.withCredentials = true;
