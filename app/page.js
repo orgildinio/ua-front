b4c2ea8022ccdcf73addf22399575813a275c952
Tuvshinbat <TUVSHINBAT596@GMAIL.COM>
2024-05-08 00:20:32 +0800

    a

--- app/page.js
+++ app/page.js
@@ -1,16 +1,17 @@
-'use client'
-import AOS from 'aos';
-import Banner from 'components/Home/Banner';
-import Header from 'components/General/Header';
-import { useEffect } from "react";
-import { useCookies } from 'react-cookie';
-import FastLink from 'components/Home/FastLink';
-import NeedLink from 'components/Home/NeedLink';
-import TopNews from 'components/Home/TopNews';
-import Media from 'components/Home/Media';
-import Books from 'components/Home/Books';
-import Partners from 'components/General/Partners';
-import HeaderSticky from 'components/General/HeaderSticky';
+"use client";
+import AOS from "aos";
+import Banner from "components/Home/Banner";
+import Header from "components/General/Header";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import FastLink from "components/Home/FastLink";
+import NeedLink from "components/Home/NeedLink";
+import TopNews from "components/Home/TopNews";
+import Media from "components/Home/Media";
+import Books from "components/Home/Books";
+import Partners from "components/General/Partners";
+import HeaderSticky from "components/General/HeaderSticky";
+import { getUpBanner } from "lib/getFetchers";
 
 export default function Page() {
   const [cookies, setCookie] = useCookies(["language"]);
@@ -18,21 +19,31 @@ export default function Page() {
   useEffect(() => {
     window.onscroll = () => {
       let header = document.querySelector(".header");
-      let headertwo = document.querySelector('.header-two')
+      let headertwo = document.querySelector(".header-two");
+      let ads = document.querySelector(".ads");
+      let fastBox = document.querySelector(".fast-boxs");
       let sticky = header.offsetTop;
+      if (window.pageYOffset >= fastBox.offsetTop) {
+        ads.classList.add("sticky-ads");
+      } else {
+        ads.classList.remove("sticky-ads");
+      }
       if (window.pageYOffset > sticky) {
         header.classList.add(`displayNone`);
-        headertwo.classList.add('header-sticky');
+        headertwo.classList.add("header-sticky");
+      } else {
+        header.classList.remove(`displayNone`);
+        headertwo.classList.remove("header-sticky");
+        ads.classList.remove("sticky-ads");
       }
-      else { header.classList.remove(`displayNone`); headertwo.classList.remove('header-sticky'); }
     };
     if (!cookies.language) setCookie("language", "mn", { path: "/" });
     AOS.init();
-  }, [])
+  }, []);
 
   return (
     <>
-      <section className='header-section'>
+      <section className="header-section">
         <Header />
         <HeaderSticky />
         <Banner />
